832412551fb775a6386f9c5fe4b8200f2c2d9060
risav-pyakurel <pyakurel.risav@gmail.com>
2024-08-16 14:09:57 +0545

    making improvements

--- cli.py
+++ cli.py
@@ -33,7 +33,6 @@ def check_admin_credentials():
         with open(ADMIN_INFO, 'r') as file:
             lines = file.readlines()
             stored_username = lines[0].strip().split(":")[1]
-            print(stored_username)
             stored_password = lines[1].strip().split(":")[1]
             if (stored_username == username) and (stored_password == password):
                 print("Welcome to the admin panel. Continue to add Questions")
@@ -44,46 +43,28 @@ def check_admin_credentials():
 def add_questions():
     with open(QUIZ_QUESTIONS,'a') as file:
         question = input("Enter the question: ")
-        file.write(f"{question}\ n")
+        file.write(f"{question}")
         Option_A = input("Enter a Option A: ")
-        file.write(f"A) {Option_A} \n ")
+        file.write(f"A) {Option_A} ")
         Option_B = input("Enter a Option B: ")
-        file.write(f" B) {Option_B} \n")
+        file.write(f" B) {Option_B}")
         Option_C = input("Enter a Option C: ")
-        file.write(f" C) {Option_C} \n")
+        file.write(f" C) {Option_C}")
         Option_D = input("Enter a Option D: ")
-        file.write(f" D) {Option_D} \n")
+        file.write(f" D) {Option_D}")
 
         answer = input("Enter the correct answer: ").upper()
         print("Question is added!!")
 
 
 
-def quiz_game():
-    print("Let's start the game!")
-    with open(QUIZ_QUESTIONS, 'r') as file:
-        lines = file.readline()
-        score = 0
-        question_num = 0
-    for i in range(0 , len(lines), 6):
-        question_num +=1
-        print(f"\n Question {question_num}: {lines[i].strip()} ")
-        print(lines[i+1].strip())
-        print(lines[i+2].strip())
-        print(lines[i+3].strip())
-        print(lines[i+4].strip())
+# def quiz_game():
+#     print("Let's start the game!")
+#     with open(QUIZ_QUESTIONS, 'r') as file:
+#         lines = file.readline()
+#         score = 0
+#         question_num = 0
 
-        user_answer = input("Choose the correct Option: ").upper()
-        correct_answer= lines[i+5].strip().split(": ")[1]
-
-        if user_answer == correct_answer:
-            print("Your answer is correct! ")
-            score +=1
-
-        else:
-            print(f"Your given answer is incorrect. The correct answer {correct_answer}!!")
-
-    print(f" Quiz game is completed!!, You Scored: {score}/{question_num}")
 
 
 def login_option():
@@ -97,11 +78,30 @@ def login_option():
             add_questions()
     elif choice == 2:
         print("Welcome to the Quiz app!!!!!!!!")
-        quiz_game()
+
 
     else:
-        print("Invalid choice of option! You don't  deserve to play this!!!!!")
+        print("Invalid choice of option! You don't  deserve to play this game!!!!!")
 
+
+def prepare_question():
+    final_question_array=[]
+    question =''
+    answer = []
+    correct_answer= ''
+    with open(QUIZ_QUESTIONS, 'r') as file:
+        for num,line in enumerate(file):
+            if (num+1)%6==1:
+                question=line
+            elif (num+1)%6>=2 and (num+1)%6<=5:
+                answer.append(line)
+            elif(num+1)%6 ==0:
+                correct_answer = line
+                one_set={'question':question,"choices":answer,"correct_answer":correct_answer}
+                final_question_array.append(one_set)
+                question=''
+                answer=[]
+                correct_answer=''
 
 def main():
     if not check_admin():
@@ -109,7 +109,11 @@ def main():
     else:
         print("Admin already exists")
     login_option()
+    prepare_question()
+
 
 
 
 main()
+
+
