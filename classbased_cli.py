6b0452472f3069b58fd13f1bfcdc6745d10ce52e
risav-pyakurel <pyakurel.risav@gmail.com>
2024-08-20 15:01:50 +0545

    finalizing the project

--- classbased_cli.py
+++ classbased_cli.py
@@ -44,6 +44,48 @@ class Admin():
                     print("Please enter the correct credentials!!!")
 
 
+class Questions:
+
+    def add_questions(self):
+        with open(QUIZ_QUESTIONS, 'a') as file:
+            question = input("Enter the question: ")
+            file.write(f" {question}")
+            Option_A = input("Enter a Option A: ")
+            file.write(f" A) {Option_A}")
+            Option_B = input("Enter a Option B: ")
+            file.write (f"{Option_B}")
+            Option_C = input("Enter a Option C: ")
+            file.write(f"{Option_C}")
+            Option_D = input("Enter a Option D: ")
+            file.write(f"{Option_D}")
+
+            answer = input("Enter a Correct answer: ").upper()
+            print(" Question is added!!")
+
+    def login_option(self):
+        print("Choose login mode as: \n")
+        print("1. Admin Login \n")
+        print("2. Continue as user \n")
+        choice = int(input("Enter login choice: \n"))
+
+        if choice == 1:
+            if self.check_admin_credentials():
+                self.add_questions()
+
+        elif choice ==2:
+            print("Welcome to quiz app!!!")
+
+        else:
+            print("Invalid option!")
+
+
+
+
+
+
+
+
+
 
 
 
